cfb9700df0900a665983e73e39f7cf98894a0f97
Harsh Srivastav <hsrivastav45@gmail.com>
2024-02-08 01:28:29 +0530

    feat:Layout Route + bug:Individual Van Route Page Layout Fix

--- components/VanItem.jsx
+++ components/VanItem.jsx
@@ -1,50 +1,60 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom";
 
 export default function VanItem() {
+  const params = useParams();
+  const [van, setVan] = React.useState({});
 
-    
-    const params = useParams();
-    const [van, setVan] = React.useState({})
-    
-    React.useEffect(() => {
-        fetch(`/api/vans/${params.id}`)
-        .then(res => res.json())
-        .then(data => setVan(data.vans))
-    }, [params.id])
-    
+  React.useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => setVan(data.vans));
+  }, [params.id]);
 
-    return (
+  return (
     <div className="each-van-route">
-        <Link to={`/vans`}className="back-to-vans">Back to all vans</Link>
-        {van.imageUrl ? (<div className="each-van-div">
+      <Link to={`/vans`} className="back-to-vans">
+        Back to all vans
+      </Link>
+      {van.imageUrl ? (
+        <div className="each-van-div">
+          <div>
             <img className="each-van-img" src={van.imageUrl} alt={van.name} />
-            <div style={
-                {
-                    backgroundColor:
-                    van.type === "Rugged" ? "#115E59" : van.type === "Luxury" 
-                        ? "#161616" : "#E17654",
-                    maxWidth: 'fit-content',
-                    color: 'white',
-                    borderRadius: '5px',
-                    padding: '0.25rem 1.25rem',
-                    fontSize: '1rem',
-                    fontWeight: '600',
-                    lineHeight: '2rem',
-                    letterSpacing: '0em',
-                    textAlign: 'center'
-
-                }
-            }>{van.type}</div>
+          </div>
+          <div>
+            <div
+              style={{
+                backgroundColor:
+                  van.type === "Rugged"
+                    ? "#115E59"
+                    : van.type === "Luxury"
+                    ? "#161616"
+                    : "#E17654",
+                maxWidth: "fit-content",
+                color: "white",
+                borderRadius: "5px",
+                padding: "0.25rem 1.25rem",
+                fontSize: "1rem",
+                fontWeight: "600",
+                lineHeight: "2rem",
+                letterSpacing: "0em",
+                textAlign: "center",
+              }}
+            >
+              {van.type}
+            </div>
             <div className="each-van-name">{van.name}</div>
             <div>
-                <span className="each-van-price">${van.price}</span> 
-                <span>/day</span> 
+              <span className="each-van-price">${van.price}</span>
+              <span>/day</span>
             </div>
             <div className="each-van-desc">{van.description}</div>
             <button className="each-van-rentBtn">Rent this Van!</button>
-        </div>)
-        : (<h3>Loading...</h3>)}
+          </div>
+        </div>
+      ) : (
+        <h3>Loading...</h3>
+      )}
     </div>
-    )
+  );
 }
--- index.jsx
+++ index.jsx
@@ -1,37 +1,36 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
-import Navbar from './components/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home'
-import About from './pages/About'
-import Vans from './pages/Vans';
-import Footer from './components/Footer';
-import { makeServer } from './server.js';
-import VanItem from './components/VanItem.jsx';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Vans from "./pages/Vans";
+import Footer from "./components/Footer";
+import { makeServer } from "./server.js";
+import VanItem from "./components/VanItem.jsx";
+import Layout from "./components/Layout.jsx";
 
-if (process.env.NODE_ENV === 'development') {
- makeServer({ environment: 'development' });
+if (process.env.NODE_ENV === "development") {
+  makeServer({ environment: "development" });
 }
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Navbar/>
-    <Suspense fallback={<h1>Loading...</h1>}>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/vans' element={<Vans/>}/>
-      <Route path='/vans/:id' element={<VanItem/>}/>
-    </Routes>
-    </Suspense>
-    </BrowserRouter>
-    <Footer/>
+      <BrowserRouter>
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <Routes>
+            <Route element={<Layout/>}>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/vans" element={<Vans />} />
+              <Route path="/vans/:id" element={<VanItem />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
