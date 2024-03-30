4e4a06e5ceb14a2008b6f2ec439ae10d8d781f92
Harsh Srivastav <hsrivastav45@gmail.com>
2024-03-30 13:04:34 +0530

    Adding Form Submit Actions + Better Auth +  Remebering current routes if logged out

--- index.jsx
+++ index.jsx
@@ -16,12 +16,12 @@ import Reviews from "./pages/host/Reviews.jsx";
 import Income from "./pages/host/Income.jsx";
 import HostLayout from "./components/HostLayout.jsx";
 import HostVans, {loader as hostVansLoader } from "./pages/host/HostVans.jsx";
-import HostVanDetailsLayout, { loader as hostVanDetailsLoader, loader } from "./pages/host/HostVanDetailsLayout.jsx";
+import HostVanDetailsLayout, { loader as hostVanDetailsLoader } from "./pages/host/HostVanDetailsLayout.jsx";
 import HostVanDetails from "./pages/host/HostVanDetails.jsx";
 import HostVanPricing from "./pages/host/HostVanPricing.jsx";
 import HostVanPhotos from "./pages/host/HostVanPhotos.jsx";
 import NotFound from "./pages/NotFound.jsx";
-import Login, {loader as loginLoader} from "./pages/Login.jsx";
+import Login, {loader as loginLoader, action as loginAction} from "./pages/Login.jsx";
 import Error from "./components/Error.jsx";
 
 import "./server.js";
@@ -32,7 +32,11 @@ const router = createBrowserRouter(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
-      <Route path="login" element={<Login />} loader={loginLoader}/>
+      <Route 
+        path="login" 
+        element={<Login />} 
+        loader={loginLoader}
+        action={loginAction}/>
       <Route
         path="vans"
         element={<Vans />}
@@ -51,15 +55,15 @@ const router = createBrowserRouter(
         <Route 
           index 
           element={<Dashboard />} 
-          loader={async () => await requireAuth()} />
+          loader={async ({request}) => await requireAuth(request)} />
         <Route 
           path="reviews" 
           element={<Reviews />} 
-          loader={async () => await requireAuth()} />
+          loader={async ({request}) => await requireAuth(request)} />
         <Route 
           path="income" 
           element={<Income />} 
-          loader={async () => await requireAuth()} />
+          loader={async ({request}) => await requireAuth(request)} />
         <Route 
           path="vans" 
           element={<HostVans />} 
@@ -70,9 +74,9 @@ const router = createBrowserRouter(
           element={<HostVanDetailsLayout />} 
           loader={hostVanDetailsLoader}>
             
-          <Route index element={<HostVanDetails />} loader={async () => await requireAuth()} />
-          <Route path="pricing" element={<HostVanPricing />} loader={async () => await requireAuth()}/>
-          <Route path="photos" element={<HostVanPhotos />} loader={async () => await requireAuth()}/>
+          <Route index element={<HostVanDetails />} loader={async ({request}) => await requireAuth(request)} />
+          <Route path="pricing" element={<HostVanPricing />} loader={async ({request}) => await requireAuth(request)}/>
+          <Route path="photos" element={<HostVanPhotos />} loader={async ({request}) => await requireAuth(request)}/>
         </Route>
       </Route>
     </Route>
